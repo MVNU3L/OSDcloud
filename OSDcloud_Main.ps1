d8b993131987fc696d403879436506d784c8bdfb
MVNU3L <168239589+MVNU3L@users.noreply.github.com>
2024-04-27 12:28:58 +0200

    Update OSDcloud_Main.ps1

--- OSDcloud_Main.ps1
+++ OSDcloud_Main.ps1
@@ -3,7 +3,7 @@ Import-Module OSD -Force
 Write-Host  -ForegroundColor Yellow "Starting ALN's Custom OSDCloud-Menu ..."
 Write-Host 
 Write-Host "===================== Main Menu =======================" -ForegroundColor Yellow
-Write-Host "1: Zero-Touch Win10 21H1 | German | Professional"-ForegroundColor Yellow
+Write-Host "1: Zero-Touch Win11 23H2 | German | Professional"-ForegroundColor Yellow
 Write-Host "2: Zero-Touch Win10 20H2 | German | Professional" -ForegroundColor Yellow
 Write-Host "3: Zero-Touch Win10 21H1 | English | Enterprise"-ForegroundColor Yellow
 Write-Host "4: Zero-Touch Win10 20H2 | English | Enterprise" -ForegroundColor Yellow
@@ -22,7 +22,7 @@ if ((Get-MyComputerModel) -match 'Virtual') {
 
 switch ($input)
 {
-    '1' { Start-OSDCloud -OSLanguage de-de -OSBuild 21H1 -OSEdition Pro } 
+    '1' { Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 22H2 -OSEdition Pro -OSLanguage de-de -OSLicense Retail } 
     '2' { Start-OSDCloud -OSLanguage de-de -OSBuild 20H2 -OSEdition Pro }
     '3' { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI }
     '4' { Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI } 
