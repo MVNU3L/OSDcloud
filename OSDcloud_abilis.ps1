93c32886c85dbf6211d4a05b7c6050a45b530e69
MVNU3L <168239589+MVNU3L@users.noreply.github.com>
2025-03-25 08:59:11 +0100

    Update OSDcloud_abilis.ps1

--- OSDcloud_abilis.ps1
+++ OSDcloud_abilis.ps1
@@ -130,3 +130,26 @@ $null = Stop-Transcript
 
 
 #endregion
+
+#region Windows
+if ($WindowsPhase -eq 'Windows') {
+
+#============================================
+#	Suspend BitLocker
+#   https://docs.microsoft.com/en-us/windows/security/information-protection/bitlocker/bcd-settings-and-bitlocker
+#============================================
+$BitLockerVolumes = Get-BitLockerVolume | Where-Object { ($_.ProtectionStatus -eq 'On') -and ($_.VolumeType -eq 'OperatingSystem') } -ErrorAction Ignore
+if ($BitLockerVolumes) {
+$BitLockerVolumes | Suspend-BitLocker -RebootCount 1 -ErrorAction Ignore
+    
+if (Get-BitLockerVolume -MountPoint $BitLockerVolumes | Where-Object ProtectionStatus -eq "On") {
+    Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Unable to suspend BitLocker for next boot"
+    }
+        else {
+            Write-Host -ForegroundColor DarkGray "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) BitLocker is suspended for the next boot"
+            }
+        }
+
+}
+
+#endregion
