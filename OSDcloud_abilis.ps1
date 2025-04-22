9f410d63ee231472e66865812197b876d53902d0
MVNU3L <168239589+MVNU3L@users.noreply.github.com>
2025-04-22 12:17:59 +0200

    Update OSDcloud_abilis.ps1

--- OSDcloud_abilis.ps1
+++ OSDcloud_abilis.ps1
@@ -207,7 +207,7 @@ RD C:\Drivers /S /Q
 RD C:\Temp /S /Q
 REM Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://tinyurl.com/BloatwareWindows #removes bloatware
 REM Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://tinyurl.com/SuspendBitlocker #suspend Bitlocker
-Start /Wait PowerShell -NoL -C Install-OSDCloudDriverPack
+REM Start /Wait PowerShell -NoL -C Install-OSDCloudDriverPack
 REM Start /Wait PowerShell -NoL -C Install-Module AutopilotOOBE -force
 Start /Wait PowerShell -NoL -C Start-AutopilotOOBE
 Start /Wait PowerShell -NoL -C Start-OOBEDeploy
